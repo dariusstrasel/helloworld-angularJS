36786ec6ffcc142207e71ea2364270bf27ce8120
Dstrasel <siruku@gmail.com>
2017-06-14 18:10:30 -0400

    Refactor myController to mainController

--- controller.js
+++ controller.js
@@ -1,14 +1,24 @@
 /**
  * Created by Dstrasel on 6/14/2017.
  */
-angular.module('myApp').controller('MyController', MyController).controller('AboutController', AboutController);
+angular.module('myApp').controller('MainController', MainController).controller('FilmController', FilmController);
 
-function MyController() {
+function MainController($http) {
     var vm = this;
+
+    $http.get('http://swapi-tpiros.rhcloud.com/films').then(function (response) {
+        console.log(response);
+        vm.films = response.data;
+    });
+
     vm.name = 'Darius';
 }
 
-function AboutController() {
+function FilmController($http, $routeParams) {
     var vm = this;
-    vm.about = 'This is my bio.';
+    var id = $routeParams.id;
+    $http.get('http://swapi-tpiros.rhcloud.com/films/' + id).then(function (response) {
+        console.log(response);
+        vm.film = response.data;
+    });
 }
