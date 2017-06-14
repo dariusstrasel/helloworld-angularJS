172b2fa41913428904090c4eae7a157f5595bbeb
Darius Strasel <strasel.darius@gmail.com>
2017-06-14 19:44:46 -0400

    Refactor controllers to use individual folders.

---
+++ film/film-controller.js
@@ -0,0 +1,10 @@
+angular.module("myApp").controller('FilmController', FilmController);
+
+function FilmController($http, $routeParams) {
+    var vm = this;
+    var id = $routeParams.id;
+    $http.get('http://swapi-tpiros.rhcloud.com/films/' + id).then(function (response) {
+        console.log(response);
+        vm.film = response.data;
+    });
+}
